46f972da16c286a36b2745b3dc389f4987691bc4
Madnikulin50 <madnikulin50@gmail.com>
2018-01-21 11:16:34 +0300

    initial commit

---
+++ core/io.go
@@ -0,0 +1,5 @@
+package pdu
+
+import "core"
+
+func WriteInt16(data int16, writer *core.Writer)
---
+++ security/security.go
@@ -0,0 +1,9 @@
+package gordp
+
+import (
+	"crypto/x509"
+	"crypto/rsa"
+	"crypto/md5"
+	"crypto/sha256"
+}
+
